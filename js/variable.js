347398d8fa5d10810f8003f79da873e3aa09a237
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-09-08 20:31:39 +0700

    add file variable js

---
+++ js/variable.js
@@ -0,0 +1,31 @@
+// Variable
+// variable adalah tempat untuk menyimpan data
+
+// Javascript membaca kode secara syncronus / atas ke bawah
+
+/* Tipe data
+    1. String: ''
+    2. Number: 17
+    3. Boolean: true/false
+ */
+
+//  Bedanya apa antara var, let dan const?
+/* 
+  1. Const ga bisa di reasign / tugaskan kembali
+  2. Var dia di hosting ( di angkat ke global (document) )
+  3. Let gabisa di reasign sebelum dia di deklarasikan
+*/
+
+// Mutable
+// bisa di rubah
+// Contoh:  array dan object
+
+// Imutable
+// gabisa di rubah
+// Contoh: String, number, boolean
+
+nama = 'Michael';
+
+var nama = 'Raihan';
+
+console.log(nama);
